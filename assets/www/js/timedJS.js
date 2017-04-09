f6ca038314e49319abc5699f5b0f9cebff4ae315
Ben Barnett <ben@univthink.com>
2017-04-09 17:54:33 -0400

    Update timedJS.js

--- assets/www/js/timedJS.js
+++ assets/www/js/timedJS.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
-$("#albumArtClick").dblclick(function() {
-	localStorage.clear();
+var touchtime = 0;
+$('#albumArtClick').on('click', function() {
+    if(touchtime == 0) {
+        //set first click
+        touchtime = new Date().getTime();
+    } else {
+        //compare first click to this click and see if they occurred within double click threshold
+        if(((new Date().getTime())-touchtime) < 800) {
+            //double click occurred
+            localStorage.clear();
+            touchtime = 0;
+        } else {
+            //not a double click so set as a new first click
+            touchtime = new Date().getTime();
+        }
+    } 
 });
 });
